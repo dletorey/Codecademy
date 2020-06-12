f26d9d9113610a38241ea3957189c0539b9dc4d0
Dave Letorey <daveletorey@gmail.com>
2020-06-12 16:59:25 +0100

    added a Method that outputs facts about the School

--- Classes/School-Catalogue/app.js
+++ Classes/School-Catalogue/app.js
@@ -20,4 +20,7 @@ class School {
         console.log('Invalid input: numberOfStudents must be set to a Number.')
       }
     }
+    quickFacts() {
+      console.log(`${name} educates ${numberOfStudents} students at the ${level} school level.`);
+    }
 }
