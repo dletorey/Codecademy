2095eb9635a0fa80baa42c53ba9a57de28818a6b
Dave Letorey <daveletorey@gmail.com>
2020-06-12 12:56:56 +0100

    added a Book class getters

--- Classes/Build-a-Library/app.js
+++ Classes/Build-a-Library/app.js
@@ -33,5 +33,11 @@ class Book extends Media {
       super(title);
       this._author = author;
       this._pages = pages;
-    } 
+    }
+    get author() {
+      return this._author;
+    }
+    get pages() {
+      return this._pages;
+    }
 }
