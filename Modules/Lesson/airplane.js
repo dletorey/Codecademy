d81263b71f18d97a83673da1ea370f2e4c55bd07
Dave Letorey <daveletorey@gmail.com>
2020-06-13 10:24:10 +0100

    altered airplane.js to export the variables and functions immediately they are created

--- Modules/Lesson/airplane.js
+++ Modules/Lesson/airplane.js
@@ -7,26 +7,41 @@ Airplane.availableAirplanes = [
 export default Airplane;
 */
 /* second part */
-let availableAirplanes = [
-    {
-        name: 'AeroJet',
-        fuelCapacity: 800,
-        availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'SensorOperators']
-    }, 
-    {
-        name: 'SkyJet',
-        fuelCapacity: 500,
-        availableStaff: ['pilots', 'flightAttendants']
-    }
-];
-let flightRequirements = {
-    requiredStaff: 4
+export let availableAirplanes = [{
+    name: 'AeroJet',
+    fuelCapacity: 800,
+    availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'sensorOperators'],
+    maxSpeed: 1200,
+    minSpeed: 300
+}, 
+{
+    name: 'SkyJet',
+    fuelCapacity: 500,
+    availableStaff: ['pilots', 'flightAttendants'],
+    maxSpeed: 800,
+    minSpeed: 200
+}];
+
+export let flightRequirements = {
+requiredStaff: 4,
+requiredSpeedRange: 700
 };
-const meetsStaffRequirements = (availableStaff, requiredStaff) => {
-    if (availableStaff.length >= requiredStaff) {
-        return true;
-    } else {
-        return false;
-    }
+
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
+if (availableStaff.length >= requiredStaff) {
+    return true;
+} else {
+    return false;
+}
 };
-export {availableAirplanes, flightRequirements, meetsStaffRequirements};
+
+export function meetsSpeedRangeRequirements(maxSpeed, minSpeed, requiredSpeedRange) {
+const range = maxSpeed - minSpeed;
+if (range > requiredSpeedRange) {
+    return true;
+} else {
+    return false;
+}
+}
+
+// export { availableAirplanes, flightRequirements, meetsStaffRequirements};
