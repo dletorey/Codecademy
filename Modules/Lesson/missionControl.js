300223eecb25cb8c7df5f7e0ff6c7eacfd1af06c
Dave Letorey <daveletorey@gmail.com>
2020-06-13 10:30:06 +0100

    added the new import and function

--- Modules/Lesson/missionControl.js
+++ Modules/Lesson/missionControl.js
@@ -8,7 +8,7 @@ const displayFuelCapacity = () => {
 displayFuelCapacity();
 */
 /* * Second part * */
-import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+import {availableAirplanes, flightRequirements, meetsStaffRequirements, meetsSpeedRangeRequirements} from './airplane';
 
 function displayFuelCapacity() {
   availableAirplanes.forEach(function(element) {
@@ -20,5 +20,11 @@ function displayStaffStatus() {
         console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff));
     });
 }
+function displaySpeedRangeStatus() {
+    availableAirplanes.forEach(function(element) {
+        console.log(element.name + ' meets speed range requirements: ' + meetsSpeedRangeRequirements(element.maxSpeed, element.minSpeed, flightRequirements.requiredSpeedRange));
+    });
+}
 displayFuelCapacity();
 displayStaffStatus();
+displaySpeedRangeStatus();
