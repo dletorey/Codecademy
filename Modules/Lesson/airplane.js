99b546ed6d9b0bcfa5332797dce6d526d6ab7c1a
Dave Letorey <daveletorey@gmail.com>
2020-06-13 09:53:41 +0100

    altered airplane to use named export and included a function to return required staff

--- Modules/Lesson/airplane.js
+++ Modules/Lesson/airplane.js
@@ -1,6 +1,32 @@
+/* * first part *
 let Airplane = {};
 Airplane.availableAirplanes = [
     {name: 'AeroJet', fuelCapacity: 800},
     {name: 'SkyJet', fuelCapacity: 500}
 ];
 export default Airplane;
+*/
+/* second part */
+let availableAirplanes = [
+    {
+        name: 'AeroJet',
+        fuelCapacity: 800,
+        availableStaff: ['pilots', 'flightAttendants', 'engineers', 'medicalAssistance', 'SensorOperators']
+    }, 
+    {
+        name: 'SkyJet',
+        fuelCapacity: 500,
+        availableStaff: ['pilots', 'flightAttendants']
+    }
+];
+let flightRequirements = {
+    requiredStaff: 4
+};
+const meetsStaffRequirements = (availableStaff, requiredStaff) => {
+    if (availableStaff.length >= requiredStaff) {
+        return true;
+    } else {
+        return false;
+    }
+};
+export {availableAirplanes, flightRequirements, meetsStaffRequirements};
