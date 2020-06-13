ad8b8f03cbf61221d633bc562bbbfdea58633281
Dave Letorey <daveletorey@gmail.com>
2020-06-13 10:13:05 +0100

    altered missionControll.js to use named imports

--- Modules/Lesson/missionControl.js
+++ Modules/Lesson/missionControl.js
@@ -1,3 +1,4 @@
+/* * Firts Part *
 import Airplane from './airplane';
 const displayFuelCapacity = () => {
   Airplane.availableAirplanes.forEach(function(element){
@@ -5,3 +6,19 @@ const displayFuelCapacity = () => {
   });
 };
 displayFuelCapacity();
+*/
+/* * Second part * */
+import {availableAirplanes, flightRequirements, meetsStaffRequirements} from './airplane';
+
+function displayFuelCapacity() {
+  availableAirplanes.forEach(function(element) {
+    console.log('Fuel Capacity of ' + element['name'] + ': ' + element['fuelCapacity']);
+  });
+}
+function displayStaffStatus() {
+    availableAirplanes.forEach(function(element){
+        console.log(element.name + ' meets staff requirements: ' + meetsStaffRequirements(element.availableStaff, flightRequirements.requiredStaff));
+    });
+}
+displayFuelCapacity();
+displayStaffStatus();
