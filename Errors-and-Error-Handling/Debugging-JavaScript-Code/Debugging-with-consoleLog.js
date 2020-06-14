d482e1ccfca2a06ffc3d74bdbc2b6b197ae39719
Dave Letorey <daveletorey@gmail.com>
2020-06-14 12:47:34 +0100

    fixed bugs for debugging with consoleLog

--- Errors-and-Error-Handling/Debugging-JavaScript-Code/Debugging-with-consoleLog.js
+++ Errors-and-Error-Handling/Debugging-JavaScript-Code/Debugging-with-consoleLog.js
@@ -1,14 +1,15 @@
 // Returns the string whose first letter is later in the alphabet. If both first letters are equal, returns null.
 function getLaterFirstLetter(string1, string2) {
-    const firstLetter1 = string1.charAt(1);
-    const firstLetter2 = string2.charAt(1);
+    const firstLetter1 = string1.charAt(0);
+    const firstLetter2 = string2.charAt(0);
+    // console.log(firstLetter1 + " " + firstLetter2);
     
     if (firstLetter1 === firstLetter2) {
       return null;
     } else if (firstLetter1 > firstLetter2) {
-      return string2;
-    } else {
       return string1;
+    } else {
+      return string2;
     }
   }
   
