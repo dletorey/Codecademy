ce4a5bb79f1bbb5a911b5bf0653e2c91d613c372
Dave Letorey <daveletorey@gmail.com>
2020-06-15 15:49:26 +0100

    created a function that uses Promise.all for all promises to resolve

--- Async-Await/await-promise-all/app.js
+++ Async-Await/await-promise-all/app.js
@@ -3,6 +3,24 @@ https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-java
 */
 let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js')
 // Example
-
+/*
+async function asyncPromAll() {
+  const resultArray = await Promise.all([asyncTask1(), asyncTask2(), asyncTask3(), asyncTask4()]);
+  for (let i = 0; i<resultArray.length; i++){
+    console.log(resultArray[i]); 
+  }
+}
+*/
 
 // Write your code below:
+const serveDinnerAgain = async () => {
+    let foodArray = await Promise.all([steamBroccoli(), cookRice(), bakeChicken(), cookBeans()]);
+    let vegetable = foodArray[0];
+    let starch =  foodArray[1];
+    let protein =  foodArray[2];
+    let side =  foodArray[3];
+
+    console.log(`Dinner is served. We're having ${vegetable}, ${starch}, ${protein}, and ${side}.`);
+}
+
+serveDinnerAgain();
