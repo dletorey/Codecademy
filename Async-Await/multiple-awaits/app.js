0edc596ff479e838bdac2a702417617331639f46
Dave Letorey <daveletorey@gmail.com>
2020-06-15 14:46:02 +0100

    original state of multiple awaits

---
+++ Async-Await/multiple-awaits/app.js
@@ -0,0 +1,7 @@
+/* Lesson URL
+https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-javascript/modules/asynch-js/lessons/async-await/exercises/multiple-awaits
+*/
+
+const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
+
+// Write your code below:
