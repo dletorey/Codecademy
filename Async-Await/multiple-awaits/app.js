857878de39ed59e35ab048307e89a57ca110d4d0
Dave Letorey <daveletorey@gmail.com>
2020-06-15 14:55:27 +0100

    created a function that waits for multiple promses to resolve

--- Async-Await/multiple-awaits/app.js
+++ Async-Await/multiple-awaits/app.js
@@ -5,3 +5,20 @@ https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-java
 const {shopForBeans, soakTheBeans, cookTheBeans} = require('./library.js');
 
 // Write your code below:
+// example
+/*
+async function asyncAwaitVersion() {
+    let firstValue = await returnsFirstPromise();
+    console.log(firstValue);
+    let secondValue = await returnsSecondPromise(firstValue);
+    console.log(secondValue);
+   }
+*/
+const makeBeans = async () => {
+    let type = await shopForBeans();
+    let isSoft = await soakTheBeans(type);
+    let dinner = await cookTheBeans(isSoft);
+    console.log(dinner);
+} 
+
+makeBeans();
