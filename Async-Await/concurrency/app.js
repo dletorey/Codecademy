dcb99cd343bd1f16a74ba804c5dae46109fbd1ac
Dave Letorey <daveletorey@gmail.com>
2020-06-15 15:27:52 +0100

    created a function that runs the other promises concurrently and then waits for them all to resolve before logging them

--- Async-Await/concurrency/app.js
+++ Async-Await/concurrency/app.js
@@ -1,3 +1,15 @@
+/* lesson URL
+https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-javascript/modules/asynch-js/lessons/async-await/exercises/concurrency
+*/
+
 let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js')
 
 // Write your code below:
+const serveDinner = async () => {
+    const vegetablePromise = steamBroccoli();
+    const starchPromise = cookRice();
+    const proteinPromise = bakeChicken();
+    const sidePromise =  cookBeans();
+    console.log(`Dinner is served. We're having ${await vegetablePromise}, ${await starchPromise}, ${await proteinPromise}, and ${await sidePromise}.`);
+}
+serveDinner();
