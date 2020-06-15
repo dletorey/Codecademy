d4053b60446c0db5219bdecddd28cb50366fe682
Dave Letorey <daveletorey@gmail.com>
2020-06-15 12:11:44 +0100

    corrected the promises to be chained and not nested

---
+++ Promises/avoiding-common-mistakes/app.js
@@ -0,0 +1,21 @@
+/* lesson url
+https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-javascript/modules/javascript-promises/lessons/promises/exercises/avoiding-common-mistakes
+*/
+
+const {checkInventory, processPayment, shipOrder} = require('./library.js');
+
+const order = {
+  items: [['sunglasses', 1], ['bags', 2]],
+  giftcardBalance: 79.82
+};
+
+// Refactor the code below:
+
+checkInventory(order)
+    .then((resolvedValueArray) => {
+        return processPayment(resolvedValueArray)
+    }).then((resolvedValueArray) => {
+         return shipOrder(resolvedValueArray)
+    }).then((successMessage) => {
+        console.log(successMessage);
+    });
