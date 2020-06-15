58e371ba57794156807af4c0c03aadeff998339e
Dave Letorey <daveletorey@gmail.com>
2020-06-15 11:06:42 +0100

    original state of workshop

---
+++ Promises/onfulfilled-onrejected.js
@@ -0,0 +1,9 @@
+/* url of workshop
+https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-javascript/modules/javascript-promises/lessons/promises/exercises/onfulfilled-onrejected
+*/
+
+const {checkInventory} = require('./library.js');
+
+const order = [['sunglasses', 1], ['bags', 2]];
+
+// Write your code below:
