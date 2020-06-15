66f8ded4ce9a49dde985f1334037b68d05da6350
Dave Letorey <daveletorey@gmail.com>
2020-06-15 15:30:14 +0100

    original state of await-promise-all

---
+++ Async-Await/await-promise-all/app.js
@@ -0,0 +1,8 @@
+/* lesson URL
+https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-javascript/modules/asynch-js/lessons/async-await/exercises/await-promise-all
+*/
+let {cookBeans, steamBroccoli, cookRice, bakeChicken} = require('./library.js')
+// Example
+
+
+// Write your code below:
