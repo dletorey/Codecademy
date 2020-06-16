ded6bd26d76fa2f5208946e9313485d7bb230414
Dave Letorey <daveletorey@gmail.com>
2020-06-16 09:33:27 +0100

    created a xhr response to datamuse api

--- Requests/XHR-GET-Requests-III/public/main.js
+++ Requests/XHR-GET-Requests-III/public/main.js
@@ -1,6 +1,10 @@
+/* lesson URL
+https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-javascript/modules/intermediate-javascript-requests/lessons/requests-i/exercises/xhr-get-requests-iii
+*/
+
 // Information to reach API
-
-
+const url = 'https://api.datamuse.com/words?';
+const queryParams = 'rel_rhy=';
 // Selecting page elements
 const inputField = document.querySelector('#input');
 const submit = document.querySelector('#submit');
@@ -8,7 +12,17 @@ const responseField = document.querySelector('#responseField');
 
 // AJAX function
 const getSuggestions = () => {
-
+    const wordQuery = inputField.value;
+    const endpoint = url + queryParams + wordQuery;
+    const xhr = new XMLHttpRequest;
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderResponse(xhr.response)
+        }
+    };
+    xhr.open('GET', endpoint);
+    xhr.send();
 }
 
 // Clear previous results and display results to webpage
