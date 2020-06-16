629007fa8d98087645b9885ab089c5e74812f39d
Dave Letorey <daveletorey@gmail.com>
2020-06-16 10:33:40 +0100

    created a POST XMLHttpRequest

--- Requests/XHR-Post-Requests-III/public/main.js
+++ Requests/XHR-Post-Requests-III/public/main.js
@@ -3,7 +3,7 @@ https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-java
 */
 
 // Information to reach API
-const apiKey = '<Your API Key>';
+const apiKey = '1e1e4e2918a14e2c83c6e81ca0055b5e';
 const url = 'https://api.rebrandly.com/v1/links';
 
 // Some page elements
@@ -13,6 +13,19 @@ const responseField = document.querySelector('#responseField');
 
 // AJAX functions
 const shortenUrl = () => {
+    const urlToShorten = inputField.value; 
+    const data = JSON.stringify({destination: urlToShorten});
+    const xhr = new XMLHttpRequest;
+    xhr.responseType = 'json';
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            renderRawResponse(xhr.response);
+        };
+    };
+    xhr.open('POST', url);
+    xhr.setRequestHeader('Content-type', 'application/json');
+    xhr.setRequestHeader('apikey', apiKey);
+    xhr.send(data);
 }
 
 
