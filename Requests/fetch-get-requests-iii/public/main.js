5940fe397e960036e95a9f92b2fff5096a38bfbd
Dave Letorey <daveletorey@gmail.com>
2020-06-16 11:51:48 +0100

    created the first fetch request and handled the response or error

--- Requests/fetch-get-requests-iii/public/main.js
+++ Requests/fetch-get-requests-iii/public/main.js
@@ -1,9 +1,11 @@
 /* lesson URL 
 https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-javascript/modules/intermediate-javascript-requests/lessons/requests-ii/exercises/fetch-get-requests-iii
+https://www.codecademy.com/paths/web-development/tracks/webdev-intermediate-javascript/modules/intermediate-javascript-requests/lessons/requests-ii/exercises/fetch-get-requests-iv
 */
 
 // Information to reach API
-
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
 
 // Selects page elements
 const inputField = document.querySelector('#input');
@@ -12,6 +14,14 @@ const responseField = document.querySelector('#responseField');
 
 // AJAX function
 const getSuggestions = () => {
+    const wordQuery = inputField.value;
+    const endpoint =`${url}${queryParams}${wordQuery}`;
+    fetch(endpoint, {cache: 'no-cache'}).then(response => {
+        if (response.ok) {
+           return response.json();
+        } throw new Error('Request failed!');
+    }, networkError => console.log(networkError.message)
+    ) 
 }
 
 // Clears previous results and display results to webpage
