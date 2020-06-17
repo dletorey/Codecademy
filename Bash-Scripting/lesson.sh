01a93293fca77a6bd29426a5e09eb5690432cc98
Dave Letorey <daveletorey@gmail.com>
2020-06-17 17:24:01 +0100

    added a comment about using scripts with aliases

--- Bash-Scripting/lesson.sh
+++ Bash-Scripting/lesson.sh
@@ -15,3 +15,4 @@ do
   greeting_occasion=$((greeting_occasion + 1))
 done
  
+# alias greet3="./lesson.sh 3" when added to the .bash_profile will allow you to run the script with greeting_limit set to 3
