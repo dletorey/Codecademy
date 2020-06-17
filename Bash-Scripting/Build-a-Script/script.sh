93cbe00d0c61884255274c15ca05c5767240ad64
Dave Letorey <daveletorey@gmail.com>
2020-06-17 17:47:02 +0100

    added user input to confirm continue

--- Bash-Scripting/Build-a-Script/script.sh
+++ Bash-Scripting/Build-a-Script/script.sh
@@ -5,4 +5,7 @@ echo $firstline
 read -a splitfirstline <<< $firstline
 version=${splitfirstline[1]} 
 echo $version
+echo "do you need to make a change to the version (enter "1" for yes, "0" for no)"
+read versioncontinue
+
 # project url https://www.codecademy.com/paths/web-development/tracks/learn-the-command-line/modules/bash-scripting/projects/bash-scripting-p
