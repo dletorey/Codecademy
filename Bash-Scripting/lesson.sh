a78eaa68fd0a41129014fca0068094b601657123
Dave Letorey <daveletorey@gmail.com>
2020-06-17 16:58:02 +0100

    created while loop and moved condition inside it

--- Bash-Scripting/lesson.sh
+++ Bash-Scripting/lesson.sh
@@ -2,10 +2,14 @@
 first_greeting="Nice to meet you!"
 later_greeting="How are you?"
 greeting_occasion=0
-if [ $greeting_occasion -lt 1 ]
-then
-  echo $first_greeting
-else
-  echo $later_greeting
-fi
+while [ $greeting_occasion -lt 3 ]
+do
+  if [ $greeting_occasion -lt 1 ]
+  then
+    echo $first_greeting
+  else
+    echo $later_greeting
+  fi
+  greeting_occasion=$((greeting_occasion + 1))
+done
 
