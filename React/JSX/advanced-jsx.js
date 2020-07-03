568f072a35514d34e91bfc9cd77dc0b912a24615
Dave Letorey <daveletorey@gmail.com>
2020-07-03 11:04:10 +0100

    completed 4th workshop adding curly braces makes the javascript run rather than display the text

--- React/JSX/advanced-jsx.js
+++ React/JSX/advanced-jsx.js
@@ -22,12 +22,22 @@ const profile = (
     </div>
   );
 */
-/* Workshop - JavaScript In Your JSX In Your JavaScript */
+/* Workshop - JavaScript In Your JSX In Your JavaScript 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 // Write code here:
 ReactDOM.render(
   <h1>2 + 3</h1>,
+  document.getElementById('app')
+);
+*/
+/* Workshop - Curly Braces in JSX */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+ReactDOM.render(
+  <h1>{2 + 3}</h1>,
   document.getElementById('app')
 );
