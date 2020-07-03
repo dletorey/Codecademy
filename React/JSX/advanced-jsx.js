26d90479b3a34a11423692cf9ce4f653109b9021
Dave Letorey <daveletorey@gmail.com>
2020-07-03 11:56:13 +0100

    completed workshop 12 - JSX Conditionals: && if the variable is not truthy display list item

--- React/JSX/advanced-jsx.js
+++ React/JSX/advanced-jsx.js
@@ -159,7 +159,7 @@ if (coinToss() === "heads") {
 }
 ReactDOM.render(img, document.getElementById('app'));
 */
-/* Workshop - JSX Conditionals: The Ternary Operator */
+/* Workshop - JSX Conditionals: The Ternary Operator 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -177,5 +177,40 @@ const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
 
 ReactDOM.render(
 	img, 
+	document.getElementById('app')
+);
+*/
+/* Workshop - JSX Conditionals: && */
+/* Example
+const tasty = (
+  <ul>
+    <li>Applesauce</li>
+    { !baby && <li>Pizza</li> }
+    { age > 15 && <li>Brussels Sprouts</li> }
+    { age > 20 && <li>Oysters</li> }
+    { age > 25 && <li>Grappa</li> }
+  </ul>
+);
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// judgmental will be true half the time.
+const judgmental = Math.random() < 0.5;
+
+const favoriteFoods = (
+  <div>
+    <h1>My Favorite Foods</h1>
+    <ul>
+      <li>Sushi Burrito</li>
+      <li>Rhubarb Pie</li>
+      {!judgmental && <li>Nacho Cheez Straight Out The Jar</li>}
+      <li>Broiled Grapefruit</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(
+	favoriteFoods, 
 	document.getElementById('app')
 );
