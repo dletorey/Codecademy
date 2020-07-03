8e4bf29c826ad99a1fbef2eb43628210dac3b1af
Dave Letorey <daveletorey@gmail.com>
2020-07-03 11:50:34 +0100

    completed workshop 11 JSX & Ternary Opertors

--- React/JSX/advanced-jsx.js
+++ React/JSX/advanced-jsx.js
@@ -136,7 +136,7 @@ ReactDOM.render(
   document.getElementById('app')
 );
 */
-
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -158,3 +158,24 @@ if (coinToss() === "heads") {
   img = <img src={pics.doggy} alt="dog" />
 }
 ReactDOM.render(img, document.getElementById('app'));
+*/
+/* Workshop - JSX Conditionals: The Ternary Operator */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function coinToss () {
+  // Randomly return either 'heads' or 'tails'.
+  return Math.random() < 0.5 ? 'heads' : 'tails';
+}
+
+const pics = {
+  kitty: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg',
+  doggy: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg'
+};
+
+const img = <img src={pics[coinToss() === 'heads' ? 'kitty' : 'doggy']} />;
+
+ReactDOM.render(
+	img, 
+	document.getElementById('app')
+);
