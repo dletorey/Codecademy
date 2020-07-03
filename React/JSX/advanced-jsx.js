56c6be525cc668c3a6265eca93a79d045612a3ed
Dave Letorey <daveletorey@gmail.com>
2020-07-03 10:49:00 +0100

    completed 1st 2 workshops classNames & self closing tags

---
+++ React/JSX/advanced-jsx.js
@@ -0,0 +1,23 @@
+/* url https://www.codecademy.com/paths/web-development/tracks/front-end-applications-with-react/modules/react-101-jsx-u/lessons/react-jsx-advanced/exercises/jsx-classname-class */
+/* Workshop - class vs className 
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Write code here:
+const myDiv = <div className="big">I AM A BIG DIV</div>;
+ReactDOM.render(myDiv, document.getElementById('app'))
+*/
+/* Workshop - Self-Closing Tags */
+const profile = (
+    <div>
+      <h1>I AM JENKINS</h1>
+      <img src="images/jenkins.png" />
+      <article>
+        I LIKE TO SIT
+        <br />
+        JENKINS IS MY NAME
+        <br />
+        THANKS HA LOT
+      </article>
+    </div>
+  );
