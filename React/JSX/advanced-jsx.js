73758d9a4c81ac52e98f0cfee0dac122f177a1bb
Dave Letorey <daveletorey@gmail.com>
2020-07-03 11:34:30 +0100

    completed workshop 8 Event Listeners in JSX added an onClick Event to the image

--- React/JSX/advanced-jsx.js
+++ React/JSX/advanced-jsx.js
@@ -79,7 +79,7 @@ const theBestString = 'tralalalala i am da best';
 
 ReactDOM.render(<h1>{theBestString}</h1>, document.getElementById('app'));
 */
-/* Workshop - Variable Attributes in JSX */
+/* Workshop - Variable Attributes in JSX 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -88,3 +88,23 @@ const goose = 'https://s3.amazonaws.com/codecademy-content/courses/React/react_p
 // Declare new variable here:
 const gooseImg = <img src={goose} />;
 ReactDOM.render(gooseImg,document.getElementById('app'));
+*/
+
+/* Workshop - Event Listeners in JSX */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+function makeDoggy(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  e.target.setAttribute('src', 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-puppy.jpeg');
+  e.target.setAttribute('alt', 'doggy');
+}
+
+const kitty = (
+	<img 
+		src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-kitty.jpg" 
+		alt="kitty" onClick={makeDoggy} />
+);
+
+ReactDOM.render(kitty, document.getElementById('app'));
