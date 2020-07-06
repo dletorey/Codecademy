26784d87bbced64130d80527910186fadcfc8bf7
Dave Letorey <daveletorey@gmail.com>
2020-07-06 17:39:43 +0100

    completed the 2nd workshop - Use a Variable Attribute in a Component

--- React/Components and Advanced JSX/app.js
+++ React/Components and Advanced JSX/app.js
@@ -1,3 +1,5 @@
+/* Workshop Use Multiline JSX in a Component */
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -23,3 +25,26 @@ ReactDOM.render(
   <QuoteMaker />,
   document.getElementById('app')
 );
+*/
+/* Workshop - Use a Variable Attribute in a Component */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const owl = {
+  title: 'Excellent Owl',
+  src: 'https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-owl.jpg'
+};
+
+// Component class starts here:
+class Owl extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>{owl.title}</h1>
+        <img src={owl.src} alt={owl.title} />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<Owl />, document.getElementById('app'));
