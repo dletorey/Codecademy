8132d24638295316ffd62977b6488065d7719eae
Dave Letorey <daveletorey@gmail.com>
2020-07-06 17:53:09 +0100

    completed 3rd workshop - Put Logic in a Render Function

--- React/Components and Advanced JSX/app.js
+++ React/Components and Advanced JSX/app.js
@@ -51,8 +51,8 @@ class Owl extends React.Component {
 ReactDOM.render(<Owl />, document.getElementById('app'));
 */
 /* Workshop - Put Logic in a Render Function */
-
-
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 const friends = [
     {
@@ -69,5 +69,16 @@ const friends = [
     }
   ];
   
-  // New component class starts here:
-  
+// New component class starts here:
+class Friend extends React.Component {
+    render() {
+        let friend = friends[0];
+        return (
+            <div>
+                <h1>{friend.title}</h1>
+                <img src={friend.src} />
+            </div>
+        )
+    }
+}
+ReactDOM.render(<Friend />,document.getElementById('app'));
