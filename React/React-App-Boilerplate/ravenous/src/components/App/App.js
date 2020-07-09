725364a76d63ca2f479befb85d17123494bba597
Dave Letorey <daveletorey@gmail.com>
2020-07-09 16:03:57 +0100

    removed the logo.svg file from app.js

--- React/React-App-Boilerplate/ravenous/src/components/App/App.js
+++ React/React-App-Boilerplate/ravenous/src/components/App/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList.js';
 import SearchBar from '../SearchBar/SearchBar.js';
