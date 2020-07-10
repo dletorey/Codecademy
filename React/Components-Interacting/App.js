d3ba740f1575825b60a9095babeed1f4f5349d93
Dave Letorey <daveletorey@gmail.com>
2020-07-10 11:39:12 +0100

    starting point of 5/13 Pass props From Component To Component

---
+++ React/Components-Interacting/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>
+          Hullo and, "Welcome to The Newzz," "On Line!"
+        </h1>
+        
+        <article>
+          Latest newzz:  where is my phone?
+        </article>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />, 
+  document.getElementById('app')
+);
--- React/Components-Interacting/Greeting.js
+++ React/Components-Interacting/Greeting.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 
 class Greeting extends React.Component {
   render() {
+    return <h1>Hi there, {this.props.name}!</h1>;
+  }
+}
+
+ReactDOM.render(
+  <Greeting name='Groberta' />, 
+  document.getElementById('app')
+);
+/* commented as on 5/13
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Greeting extends React.Component {
+  render() {
     return <h1>Hi there, {this.props.firstName}</h1>;
   }
 }
@@ -11,3 +25,4 @@ ReactDOM.render(
   <Greeting firstName='Groberta' />, 
   document.getElementById('app')
 );
+*/
