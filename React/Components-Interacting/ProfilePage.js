b292e8cb8bbf64f42a08eac59965fd39cc1014f9
Dave Letorey <daveletorey@gmail.com>
2020-07-10 10:47:11 +0100

    made the profile page component render the NavBar component

--- React/Components-Interacting/ProfilePage.js
+++ React/Components-Interacting/ProfilePage.js
@@ -6,7 +6,7 @@ class ProfilePage extends React.Component {
   render() {
     return (
       <div>
-
+        <NavBar />
         <h1>All About Me!</h1>
         <p>I like movies and blah blah blah blah blah</p>
         <img src="https://s3.amazonaws.com/codecademy-content/courses/React/react_photo-monkeyselfie.jpg" />
