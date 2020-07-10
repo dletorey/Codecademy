97034cb3d5aa5e056f96a3662464318cf1ab425e
Dave Letorey <daveletorey@gmail.com>
2020-07-10 16:53:30 +0100

    start of 12/13 defaultProps

--- React/Components-Interacting/Button.js
+++ React/Components-Interacting/Button.js
@@ -1,4 +1,26 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button>
+        {this.props.text}
+      </button>
+    );
+  }
+}
+
+// defaultProps goes here:
+
+
+ReactDOM.render(
+  <Button />, 
+  document.getElementById('app')
+);
+
+/*
+import React from 'react';
 
 export class Button extends React.Component {
   render() {
@@ -9,3 +31,4 @@ export class Button extends React.Component {
     );
   }
 }
+*/
