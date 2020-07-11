df98d91b999a154c97278e84c1953e43b9346435
Dave Letorey <daveletorey@gmail.com>
2020-07-11 17:19:46 +0100

    called the default/starting state and rendered the component

--- React/Components-Interacting/this-state/App.js
+++ React/Components-Interacting/this-state/App.js
@@ -11,8 +11,9 @@ class App extends React.Component {
   render() {
     return (
       <h1>
-        Wow this entire app is just an h1.
+        {this.state.title}
       </h1>
     );
   }
 }
+ReactDOM.render(<App />, document.getElementById('app'));
