b3d750f5a8dd83c575aab811e9ce2180e4936258
Dave Letorey <daveletorey@gmail.com>
2020-07-12 12:39:59 +0100

    moved the business object from Business.js to App.js

--- React/React-App-Boilerplate/ravenous/src/components/App/App.js
+++ React/React-App-Boilerplate/ravenous/src/components/App/App.js
@@ -2,7 +2,17 @@ import React from 'react';
 import './App.css';
 import BusinessList from '../BusinessList/BusinessList.js';
 import SearchBar from '../SearchBar/SearchBar.js';
-
+const business = {
+  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
 function App() {
   return (
     <div className="App">
