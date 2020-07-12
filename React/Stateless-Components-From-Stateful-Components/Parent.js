c2d4cbf416570b5a0a3820d4ee331a39ebf3b931
Dave Letorey <daveletorey@gmail.com>
2020-07-12 19:57:20 +0100

    added a connstructor with default state to Parent and created aChild component

--- React/Stateless-Components-From-Stateful-Components/Parent.js
+++ React/Stateless-Components-From-Stateful-Components/Parent.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 
 
 class Parent extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: 'Frarthur'
+        }
+    }
     render() {
         return <div></div>;
     }
