c1ab02bb67dc8083826a2bc1eba6edee09a6bc43
Dave Letorey <daveletorey@gmail.com>
2020-07-13 17:06:52 +0100

    Starting point of Child Components Update Sibling Components

--- React/Stateless-Components-From-Stateful-Components/Child.js
+++ React/Stateless-Components-From-Stateful-Components/Child.js
@@ -1,4 +1,36 @@
+/* Child Components Update Sibling Components */
+import React from 'react';
+
+export class Child extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    const name = e.target.value;
+    this.props.onChange(name);
+  }
+
+  render() {
+    return (
+      <div>
+        <select
+          id="great-names"
+          onChange={this.handleChange}>
+
+          <option value="Frarthur">Frarthur</option>
+          <option value="Gromulus">Gromulus</option>
+          <option value="Thinkpiece">Thinkpiece</option>
+        </select>
+      </div>
+    );
+  }
+}
+
 /* Child Components Update their Parents' State */
+/*
 import React from 'react';
 
 export class Child extends React.Component {
@@ -33,7 +65,7 @@ export class Child extends React.Component {
     );
   }
 }
-
+*/
 /* 
 import React from 'react';
 
--- React/Stateless-Components-From-Stateful-Components/Parent.js
+++ React/Stateless-Components-From-Stateful-Components/Parent.js
@@ -1,4 +1,43 @@
+/* Child Components Update Sibling Components */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Child } from './Child';
+import { Sibling } from './Sibling';
+
+class Parent extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { name: 'Frarthur' };
+
+    this.changeName = this.changeName.bind(this);
+  }
+
+  changeName(newName) {
+    this.setState({
+      name: newName
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Child 
+          name={this.state.name} 
+          onChange={this.changeName} />
+        <Sibling />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Parent />,
+  document.getElementById('app')
+);
+
 /* Child Components Update their Parents' State */
+/*
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Child } from './Child';
@@ -25,7 +64,7 @@ ReactDOM.render(
 	<Parent />,
 	document.getElementById('app')
 );
-
+*/
 /* 
 import React from 'react';
 import ReactDOM from 'react-dom';
