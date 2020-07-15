fa541d6746fe9dc902cfb0bcdf2342fc0a68c187
Dave Letorey <daveletorey@gmail.com>
2020-07-15 12:51:26 +0100

    change App to be a component rather than a function

--- React/React-App-Boilerplate/ravenous/src/components/App/App.js
+++ React/React-App-Boilerplate/ravenous/src/components/App/App.js
@@ -14,14 +14,16 @@ const business = {
   reviewCount: 90
 }
 const businesses = [business, business, business, business, business, business]
-function App() {
-  return (
-    <div className="App">
-      <h1>ravenous</h1>
-      <SearchBar />
-      <BusinessList businesses={businesses} /> 
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList businesses={businesses} /> 
+      </div>
+    );
+  }
 }
 
 export default App;
