2258062465bd31a35d555eaa9e85f167f7e24988
Dave Letorey <daveletorey@gmail.com>
2020-07-15 15:36:16 +0100

    added the fetch url proceeded by the cors-anywhere url

--- React/React-App-Boilerplate/ravenous/src/util/Yelp.js
+++ React/React-App-Boilerplate/ravenous/src/util/Yelp.js
@@ -1,2 +1,6 @@
 const apiKey = 'BYWzqrEw2C7q1Uh7Dr9GHZ6nGZBxXW7n3P53dKtFJSXQbPuyS1hsDSkLcegjXnhViE0De2sqI9Ekyyf_QCHUNR1KccjULzVIdgDy9effYIPPPK8SW5tL7JCt1hAPX3Yx';
-const Yelp = {};
+const Yelp = {
+    search: (term, location, sortBy) => {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`);
+    }
+};
