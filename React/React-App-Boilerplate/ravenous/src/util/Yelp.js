61abaa2407054fc88b2309eb7bfcdf6cb3c1b869
Dave Letorey <daveletorey@gmail.com>
2020-07-15 16:02:51 +0100

    logged all the attributes that we want to use inside the map and then exported the Yelp module

--- React/React-App-Boilerplate/ravenous/src/util/Yelp.js
+++ React/React-App-Boilerplate/ravenous/src/util/Yelp.js
@@ -10,9 +10,19 @@ const Yelp = {
         }).then(jsonResponse => {
             if (jsonResponse.businesses) {
                 return jsonResponse.businesses.map(business => {
-                    
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipcode: business.location.zip_code,
+                    category: business.categories,
+                    rating: business.rating,
+                    reviewCount: business.review_count
                 })
             }
         });
     }
 };
+export default Yelp
