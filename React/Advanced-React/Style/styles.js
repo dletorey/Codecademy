dabc4f5d6afc002b265880ad055c4c4c7f1e0528
Dave Letorey <daveletorey@gmail.com>
2020-07-16 12:23:41 +0100

    starting point of SHaring Styles Across Multiple components 6/6

---
+++ React/Advanced-React/Style/styles.js
@@ -0,0 +1,5 @@
+const fontFamily = 'Comic Sans MS, Lucida Handwriting, cursive';
+const background = 'pink url("https://codecademy-content.s3.amazonaws.com/programs/react/images/welcome-to-my-homepage.gif") fixed';
+const fontSize = '4em';
+const padding = '45px 0';
+const color = 'green';
