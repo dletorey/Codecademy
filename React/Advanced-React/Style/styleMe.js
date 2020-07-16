9c0187c835e9d04be3f2a48988cab9ebbc12c501
Dave Letorey <daveletorey@gmail.com>
2020-07-16 11:21:32 +0100

    added a style to a h1 using dirty css in JavaScript. When this is no longer a course I shall only apply styles via classes and not inside a JavaScript file.

--- React/Advanced-React/Style/styleMe.js
+++ React/Advanced-React/Style/styleMe.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const styleMe = <h1>Please style me! I am so bland!</h1>;
+const styleMe = <h1 style={{background: 'lightblue', color: 'darkred'}}>Please style me! I am so bland!</h1>;
 
 ReactDOM.render(
 	styleMe, 
