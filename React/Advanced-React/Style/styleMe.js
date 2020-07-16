3f9fbc0b5e3e0cff6b43229a9d33c0f677a79bcd
Dave Letorey <daveletorey@gmail.com>
2020-07-16 11:18:13 +0100

    starting point of Advanced React - Styles

---
+++ React/Advanced-React/Style/styleMe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const styleMe = <h1>Please style me! I am so bland!</h1>;
+
+ReactDOM.render(
+	styleMe, 
+	document.getElementById('app')
+);
