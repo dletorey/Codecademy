7f62fd338403b1ca146f85cd920913b37824a033
Dave Letorey <daveletorey@gmail.com>
2020-07-16 12:10:15 +0100

    added new style names that would in CSS have a dash and made them camelCase, this is hideous and definitely just use real CSS

--- React/Advanced-React/Style/styleMe.js
+++ React/Advanced-React/Style/styleMe.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 const styles = {
     background: 'lightblue',
-    color: 'darkred'
+    color: 'darkred',
+    marginTop: '100px',
+    fontSize: '50px'
 };
 const styleMe = <h1 style={styles}>Please style me! I am so bland!</h1>;
 
