5d9cad69f82ae21d1c8850746682fe7abd869845
Dave Letorey <daveletorey@gmail.com>
2020-07-19 13:15:45 +0100

    starting point of Stateless Functional Components and Props

--- React/Advanced-React/Stateless-Functional-Components/Example.js
+++ React/Advanced-React/Stateless-Functional-Components/Example.js
@@ -1,3 +1,31 @@
+/* Stateless Functional Components and Props */
+// Normal way to display a prop:
+export class MyComponentClass extends React.Component {
+    render() {
+      return <h1>{this.props.title}</h1>;
+    }
+  }
+  
+  // Stateless functional component way to display a prop:
+  export const MyComponentClass = (props) => {
+    return <h1>{props.title}</h1>;
+  }
+  
+  // Normal way to display a prop using a variable:
+  export class MyComponentClass extends React.component {
+    render() {
+        let title = this.props.title;
+      return <h1>{title}</h1>;
+    }
+  }
+  
+  // Stateless functional component way to display a prop using a variable:
+  export const MyComponentClass = (props) => {
+      let title = props.title;
+    return <h1>{title}</h1>;
+  }
+
+/*
 // A component class written in the usual way:
 export class MyComponentClass extends React.Component {
     render() {
@@ -15,3 +43,4 @@ export class MyComponentClass extends React.Component {
       <MyComponentClass />,
       document.getElementById('app')
   );
+*/
