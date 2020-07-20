9ca4866071f9a6e368e29b83d798aeff19606610
Dave Letorey <daveletorey@gmail.com>
2020-07-20 17:01:00 +0100

    created the App component and included a render method

--- React/jamming/src/components/App/App.js
+++ React/jamming/src/components/App/App.js
@@ -4,3 +4,19 @@ import { SearchBar } from '../SearchBar/SearchBar';
 import { SearchResults } from '../SearchResults/SearchResults';
 import { Playlist } from '../Playlist/Playlist';
 
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Ja<span class="highlight">mmm</span>ing</h1>
+                <div class="App">
+                    <SearchBar />
+                    <div class="App-playlist">
+                        <SearchResults />
+                        <Playlist />
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
