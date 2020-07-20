487388324d1bf78c41dde180f96a660e442c615f
Dave Letorey <daveletorey@gmail.com>
2020-07-20 16:55:30 +0100

    added a background image, the css and imported the relevant things into App.js

---
+++ React/jamming/src/components/App/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { SearchBar } from '../SearchBar/SearchBar';
+import { SearchResults } from '../SearchResults/SearchResults';
+import { Playlist } from '../Playlist/Playlist';
+
