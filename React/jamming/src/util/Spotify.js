4e96ab46356775f26fdd2d888da0c8a3efcf0264
Dave Letorey <daveletorey@gmail.com>
2020-07-21 21:25:09 +0100

    created a condition to check if the url contains a access token ond expire time and if so sets the access token from it

--- React/jamming/src/util/Spotify.js
+++ React/jamming/src/util/Spotify.js
@@ -1,7 +1,17 @@
+const clientId = '96697afbf42141368bfb62a1d85c31bd';
 let usersAccessToken;
 const Spotify = {
     getAccessToken() {
         if (usersAccessToken) {
+            return usersAccessToken;
+        } 
+        const usersAccessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
+        if (usersAccessTokenMatch && expiresInMatch) {
+            usersAccessToken = usersAccessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => usersAccessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
             return usersAccessToken;
         }
     }
