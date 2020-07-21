d3a0d42d46f73fd8f235ec35e4032cdf0e1f9e24
Dave Letorey <daveletorey@gmail.com>
2020-07-21 21:03:53 +0100

    created a method that checks if there is a usersAccessToken and if so returns it

--- React/jamming/src/util/Spotify.js
+++ React/jamming/src/util/Spotify.js
@@ -1,6 +1,10 @@
 let usersAccessToken;
 const Spotify = {
-
+    getAccessToken() {
+        if (usersAccessToken) {
+            return usersAccessToken;
+        }
+    }
 };
 
 export default Spotify;
