ffbf0c0096b8320cef1b4d6ce52287a7526f11bb
Dave Letorey <daveletorey@gmail.com>
2020-07-22 08:37:28 +0100

    created a method called search that uses a term to fetch a list of tracks from spotify

--- React/jamming/src/util/Spotify.js
+++ React/jamming/src/util/Spotify.js
@@ -1,3 +1,5 @@
+import SearchBar from "../components/SearchBar/SearchBar";
+
 const clientId = '96697afbf42141368bfb62a1d85c31bd';
 const redirectURI = 'http://localhost:3000/'
 let usersAccessToken;
@@ -16,8 +18,18 @@ const Spotify = {
             return usersAccessToken;
         } else {
             const accessUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+            // @ts-ignore
             window.location = accessUrl;
         }
+    },
+    search(term) {
+        const usersAccessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`,{
+            headers: {
+                Authorization: `Bearer ${usersAccessToken}`
+            }
+        });
+
     }
 };
 
